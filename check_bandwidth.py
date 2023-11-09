7404f6fbf2b2bd9991a7797e53934e8d44d50b4b
Kihy <kh695640204@gmail.com>
2023-11-09 12:21:20 +1000

    add check bandwidth

---
+++ check_bandwidth.py
@@ -0,0 +1,24 @@
+from scapy.all import *
+
+live=True 
+if live:
+    iterable=sniff(iface='eth0')
+else:
+    filename="../Cam_1_ps_replay.pcapng"
+    iterable=rdpcap(filename)
+
+attacker_ip="192.168.0.199"
+victim_ip='192.168.0.131'
+
+attacker_count=0
+other_count=0
+
+for pkt in iterable:
+    if pkt.haslayer(IP):
+        if pkt[IP].dst == victim_ip:
+            if pkt[IP].src == attacker_ip:
+                attacker_count+=1
+            else:
+                other_count+=1
+
+print("occupied bandwidth", attacker_count/(attacker_count+other_count))
